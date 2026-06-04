561dbd8a0aa36a554641e9f476be69d05d367bad
John Keller <mail@johnkeller101.com>
2026-06-04 11:12:25 -0600

    Remove willTerminate observer that caused crash on launch

--- RembgMac/Models/AppState.swift
+++ RembgMac/Models/AppState.swift
@@ -149,14 +149,6 @@ final class AppState: ObservableObject {
             }
         }
 
-        // Ensure child processes are killed when the app terminates (including force-quit)
-        NotificationCenter.default.addObserver(
-            forName: NSApplication.willTerminateNotification,
-            object: nil, queue: .main
-        ) { [weak self] _ in
-            self?.processManager.stop()
-            self?.proxyServer.stop()
-        }
     }
 
     func startServer() {
